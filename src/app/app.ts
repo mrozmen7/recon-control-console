4138b7d8d2604560a651b40311a1daed1ee686c1
Yavuz Ozmen <mrozmen7@gmail.com>
2026-06-17 18:56:52 +0200

    feat: introduce component based app shell

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { AppShell } from './layout/app-shell';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AppShell],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('Recon Control Console');
-}
+export class App {}
